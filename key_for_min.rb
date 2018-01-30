5e70e5c72b64622f204100401edbf0b48161b44b
Veronica Kim <vero.a.kim@gmail.com>
2018-01-30 03:07:06 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
 
   name_hash.each do |thing, low|
     if lowest == nil || low < lowest
-      lowest == low
+      lowest = low
       lowest_thing = thing
     end
   end
