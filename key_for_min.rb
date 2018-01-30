1c3741746b3cd465faa7f8457253e58c0f2f4b4b
Veronica Kim <vero.a.kim@gmail.com>
2018-01-30 03:00:35 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   lowest_key = nil
   lowest_value = nil
 
-  name_hash.each do |key, value|
+  name_hash.each do |thing, cost|
     if lowest_value = nil || value < lowest_value
       lowest_value = value
       lowest_key = key
